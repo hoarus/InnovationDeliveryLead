9b21c6546d5cb3e8236122461af943ed18e1fe2d
hoarus <68991598+hoarus@users.noreply.github.com>
2022-01-27 16:14:59 +1000

    Implement card-swiping functionality

--- cards.js
+++ cards.js
@@ -23,46 +23,92 @@ rightArrow.addEventListener('click', cardUp);
     console.log("CardUp");
     rightArrow.classList.add("selected")
     hideAllCards();
-    toggleCard();
+    toggleCard("right");
     checkIfHideButton();
+    cards.forEach(card => card.addEventListener('transitionend', removeTransition));
   }
 
   function cardDown() {
     activeCard -= 1;
     console.log("CardUp");
     hideAllCards();
-    toggleCard();
+    toggleCard("left");
     checkIfHideButton();
+    cards.forEach(card => card.addEventListener('transitionend', removeTransition));
+  }
+
+  function removeTransition(e){
+    if (e.propertyName !== 'transform') return; //Skip if not a transform
+    console.log("over");
+    this.classList.remove('transitioning');
+    this.classList.remove('transition-left');
+    this.classList.remove('transition-right');
   }
 
 
   function hideAllCards() {
     console.log("Hiding");
     cards.forEach((card) => {
-      card.classList.add("hidden");
+      card.classList.add("hidden-card");
     })
   }
 
-  function toggleCard () {
-    console.log(activeCard);
+  function toggleCard (direction) {
+    console.log(direction);
     switch(activeCard) {
       case 1:
-        card1.classList.remove('hidden'); 
+        card1.classList.remove('hidden-card');
+        card1.classList.add('transitioning');
+        if (direction === "left") {
+          card1.classList.add("transition-left");
+        } else {
+          card1.classList.add("transition-right")
+        }
         break;
       case 2:
-        card2.classList.remove('hidden'); 
+        card2.classList.remove('hidden-card');
+        card2.classList.add('transitioning'); 
+        if (direction === "left") {
+          card2.classList.add("transition-left");
+        } else {
+          card2.classList.add("transition-right")
+        }
         break;
       case 3:
-        card3.classList.remove('hidden'); 
+        card3.classList.remove('hidden-card');
+        card3.classList.add('transitioning');
+        if (direction === "left") {
+          card3.classList.add("transition-left");
+        } else {
+          card3.classList.add("transition-right")
+        } 
         break;
       case 4:
-        card4.classList.remove('hidden'); 
+        card4.classList.remove('hidden-card');
+        card4.classList.add('transitioning');
+        if (direction === "left") {
+          card4.classList.add("transition-left");
+        } else {
+          card4.classList.add("transition-right")
+        } 
         break;
       case 5:
-        card5.classList.remove('hidden'); 
+        card5.classList.remove('hidden-card');
+        card5.classList.add('transitioning');
+        if (direction === "left") {
+          card5.classList.add("transition-left");
+        } else {
+          card5.classList.add("transition-right")
+        } 
         break;
       case 6:
-        card6.classList.remove('hidden'); 
+        card6.classList.remove('hidden-card');
+        card6.classList.add('transitioning');
+        if (direction === "left") {
+          card6.classList.add("transition-left");
+        } else {
+          card6.classList.add("transition-right")
+        } 
         break;        
     }
   }
